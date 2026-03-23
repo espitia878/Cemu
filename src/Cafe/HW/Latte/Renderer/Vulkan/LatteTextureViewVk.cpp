761bdaa6b8fd59dbe23909904ed439cc2f22da48
espitia878 <davidespitia878@gmail.com>
2026-03-23 15:12:00 -0500

    prueba final

--- src/Cafe/HW/Latte/Renderer/Vulkan/LatteTextureViewVk.cpp
+++ src/Cafe/HW/Latte/Renderer/Vulkan/LatteTextureViewVk.cpp
@@ -1,43 +1,52 @@
 #include "Cafe/HW/Latte/Renderer/Vulkan/LatteTextureViewVk.h"
 #include "Cafe/HW/Latte/Renderer/Vulkan/LatteTextureVk.h"
 #include "Cafe/HW/Latte/Renderer/Vulkan/VulkanRenderer.h"
-#include "Cafe/HW/Latte/Renderer/Vulkan/VulkanAPI.h"
+#include <algorithm>
 
 LatteTextureViewVk::LatteTextureViewVk(VkDevice device, LatteTextureVk* tex, Latte::E_DIM dim, Latte::E_GX2SURFFMT format, uint32 firstMip, uint32 mipCount, uint32 firstSlice, uint32 sliceCount)
-	: LatteTextureView(tex, dim, format, firstMip, mipCount, firstSlice, sliceCount), m_device(device)
+	: LatteTextureView(tex, dim, format, firstMip, mipCount, firstSlice, sliceCount)
+	, m_device(device)
 {
+	// Inicializamos las variables que faltaban
+	auto vkObj = tex->GetVKRObject();
+	m_format = vkObj->m_format;
+	m_uniqueId = 0; // O la lógica de ID que use tu rama
+
 	VkImageViewCreateInfo viewInfo{};
 	viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-	
-	// Acceso seguro al objeto de textura
-	auto vkObj = tex->GetVKRObject();
 	viewInfo.image = vkObj->m_image;
 	
 	switch (dim)
 	{
-	case Latte::E_DIM::DIM_1D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_1D; break;
-	case Latte::E_DIM::DIM_2D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
-	case Latte::E_DIM::DIM_2D_MSAA: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
-	case Latte::E_DIM::DIM_3D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_3D; break;
-	case Latte::E_DIM::DIM_CUBE: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_CUBE; break;
-	default: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
+		case Latte::E_DIM::DIM_1D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_1D; break;
+		case Latte::E_DIM::DIM_2D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
+		case Latte::E_DIM::DIM_2D_MSAA: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
+		case Latte::E_DIM::DIM_3D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_3D; break;
+		case Latte::E_DIM::DIM_CUBE: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_CUBE; break;
+		default: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
 	}
 
-	viewInfo.format = vkObj->m_format;
+	viewInfo.format = m_format;
 	viewInfo.subresourceRange.aspectMask = vkObj->m_imageAspect;
 	viewInfo.subresourceRange.baseMipLevel = firstMip;
 	viewInfo.subresourceRange.levelCount = mipCount;
 	viewInfo.subresourceRange.baseArrayLayer = firstSlice;
 	viewInfo.subresourceRange.layerCount = sliceCount;
 
-	// IMPORTANTE: Se usa m_view porque así está en el .h
-	if (vkCreateImageView(m_device, &viewInfo, nullptr, &m_view) != VK_SUCCESS) {
-		// Error silencioso para evitar crashes en logs
-	}
+	vkCreateImageView(m_device, &viewInfo, nullptr, &m_view);
 }
 
 LatteTextureViewVk::~LatteTextureViewVk()
 {
 	if (m_view != VK_NULL_HANDLE)
 		vkDestroyImageView(m_device, m_view, nullptr);
+}
+
+void LatteTextureViewVk::AddDescriptorSetReference(struct VkDescriptorSetInfo* dsInfo) {
+    if (std::find(list_descriptorSets.begin(), list_descriptorSets.end(), dsInfo) == list_descriptorSets.end()) 
+        list_descriptorSets.emplace_back(dsInfo);
+}
+
+void LatteTextureViewVk::RemoveDescriptorSetReference(struct VkDescriptorSetInfo* dsInfo) {
+    list_descriptorSets.erase(std::remove(list_descriptorSets.begin(), list_descriptorSets.end(), dsInfo), list_descriptorSets.end());
 }
