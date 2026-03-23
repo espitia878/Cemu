96836d822424f989259e56f0f3d77efbb8e5933a
espitia878 <davidespitia878@gmail.com>
2026-03-23 13:32:48 -0500

    TextureViewVk.cpp

--- src/Cafe/HW/Latte/Renderer/Vulkan/LatteTextureViewVk.cpp
+++ src/Cafe/HW/Latte/Renderer/Vulkan/LatteTextureViewVk.cpp
@@ -1,73 +1,39 @@
-#include "Cafe/HW/Latte/Renderer/Vulkan/LatteTextureVk.h"
 #include "Cafe/HW/Latte/Renderer/Vulkan/LatteTextureViewVk.h"
+#include "Cafe/HW/Latte/Renderer/Vulkan/LatteTextureVk.h"
 #include "Cafe/HW/Latte/Renderer/Vulkan/VulkanRenderer.h"
 #include "Cafe/HW/Latte/Renderer/Vulkan/VulkanAPI.h"
 
-LatteTextureVk::LatteTextureVk(VulkanRenderer* vkRenderer, Latte::E_DIM dim, MPTR physAddress, MPTR physMipAddress, Latte::E_GX2SURFFMT format, uint32 width, uint32 height, uint32 depth, uint32 pitch, uint32 mipLevels, uint32 swizzle, Latte::E_HWTILEMODE tileMode, bool isDepth)
-	: LatteTexture(dim, physAddress, physMipAddress, format, width, height, depth, pitch, mipLevels, swizzle, tileMode, isDepth), m_vkr(vkRenderer)
+LatteTextureViewVk::LatteTextureViewVk(VkDevice device, LatteTextureVk* tex, Latte::E_DIM dim, Latte::E_GX2SURFFMT format, uint32 firstMip, uint32 mipCount, uint32 firstSlice, uint32 sliceCount)
+	: LatteTextureView(tex, dim, format, firstMip, mipCount, firstSlice, sliceCount), m_device(device)
 {
-	vkObjTex = new VKRObjectTexture();
-	VkImageCreateInfo imageInfo{};
-	imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+	VkImageViewCreateInfo viewInfo{};
+	viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+	viewInfo.image = tex->vkObjTex->m_image;
 	
-	uint32 effWidth = (overwriteInfo.hasResolutionOverwrite) ? overwriteInfo.width : width;
-	uint32 effHeight = (overwriteInfo.hasResolutionOverwrite) ? overwriteInfo.height : height;
-	uint32 effDepth = (overwriteInfo.hasResolutionOverwrite) ? overwriteInfo.depth : depth;
-	effDepth = (effDepth > 1) ? effDepth : 1;
-
-	imageInfo.extent.width = effWidth;
-	imageInfo.extent.height = effHeight;
-	imageInfo.mipLevels = mipLevels;
-	imageInfo.usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
-
-	// PARCHE ZOMBIES (Mali Immortalis / Procesador Dimensity 9300+)
-	bool isForbidden = (format == (Latte::E_GX2SURFFMT)0x3b || format == (Latte::E_GX2SURFFMT)0x38);
-	if (!isForbidden) imageInfo.usage |= VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT;
-	
-	imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-	imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
-
-	if (dim == Latte::E_DIM::DIM_3D) {
-		imageInfo.extent.depth = effDepth;
-		imageInfo.arrayLayers = 1;
-		imageInfo.flags |= VK_IMAGE_CREATE_2D_ARRAY_COMPATIBLE_BIT;
-	} else {
-		imageInfo.extent.depth = 1;
-		imageInfo.arrayLayers = effDepth;
-	}
-	
-	VulkanRenderer::FormatInfoVK fInfo;
-	m_vkr->GetTextureFormatInfoVK(format, isDepth, dim, effWidth, effHeight, &fInfo);
-	imageInfo.format = fInfo.vkImageFormat;
-	vkObjTex->m_imageAspect = fInfo.vkImageAspect;
-	
-	if (!isDepth) {
-		imageInfo.flags |= VK_IMAGE_CREATE_MUTABLE_FORMAT_BIT;
-		if (!isForbidden) imageInfo.usage |= VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
-	} else {
-		imageInfo.usage |= VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+	switch (dim)
+	{
+	case Latte::E_DIM::DIM_1D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_1D; break;
+	case Latte::E_DIM::DIM_2D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
+	case Latte::E_DIM::DIM_2D_MSAA: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
+	case Latte::E_DIM::DIM_3D: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_3D; break;
+	case Latte::E_DIM::DIM_CUBE: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_CUBE; break;
+	default: viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D; break;
 	}
 
-	imageInfo.imageType = (dim == Latte::E_DIM::DIM_1D) ? VK_IMAGE_TYPE_1D : ((dim == Latte::E_DIM::DIM_3D) ? VK_IMAGE_TYPE_3D : VK_IMAGE_TYPE_2D);
+	viewInfo.format = tex->vkObjTex->m_format;
+	viewInfo.subresourceRange.aspectMask = tex->vkObjTex->m_imageAspect;
+	viewInfo.subresourceRange.baseMipLevel = firstMip;
+	viewInfo.subresourceRange.levelCount = mipCount;
+	viewInfo.subresourceRange.baseArrayLayer = firstSlice;
+	viewInfo.subresourceRange.layerCount = sliceCount;
 
-	if (vkCreateImage(m_vkr->GetLogicalDevice(), &imageInfo, nullptr, &vkObjTex->m_image) != VK_SUCCESS)
-		m_vkr->UnrecoverableError("Failed to create image");
-	
-	vkObjTex->m_flags = imageInfo.flags;
-	vkObjTex->m_format = imageInfo.format;
-
-	m_layoutsMips = (mipLevels > 1) ? mipLevels : 1;
-	m_layoutsDepth = (effDepth > 1) ? effDepth : 1;
-	m_layouts.resize(m_layoutsMips * m_layoutsDepth, VK_IMAGE_LAYOUT_UNDEFINED);
+	if (vkCreateImageView(m_device, &viewInfo, nullptr, &m_view) != VK_SUCCESS) {
+		// Error handling
+	}
 }
 
-LatteTextureVk::~LatteTextureVk() {
-	if (vkObjTex) delete vkObjTex;
-}
-
-void LatteTextureVk::AllocateOnHost() { }
-
-// ESTA ES LA LÍNEA QUE DEBE COINCIDIR AL 100% CON EL .H
-LatteTextureView* LatteTextureVk::CreateView(Latte::E_DIM dim, Latte::E_GX2SURFFMT format, uint32 firstMip, uint32 mipCount, uint32 firstSlice, uint32 sliceCount) {
-    return new LatteTextureViewVk(m_vkr->GetLogicalDevice(), this, dim, format, firstMip, mipCount, firstSlice, sliceCount);
+LatteTextureViewVk::~LatteTextureViewVk()
+{
+	if (m_view != VK_NULL_HANDLE)
+		vkDestroyImageView(m_device, m_view, nullptr);
 }
