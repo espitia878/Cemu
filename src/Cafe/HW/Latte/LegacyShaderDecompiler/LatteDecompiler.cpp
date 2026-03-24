d637aa67322b5349418785d3eba15d29018a7d97
espitia878 <davidespitia878@gmail.com>
2026-03-24 08:28:16 -0500

    Update LatteDecompiler.cpp

--- src/Cafe/HW/Latte/LegacyShaderDecompiler/LatteDecompiler.cpp
+++ src/Cafe/HW/Latte/LegacyShaderDecompiler/LatteDecompiler.cpp
@@ -1090,7 +1090,9 @@ void LatteDecompiler_InitContext(LatteDecompilerShaderContext& dCtx, const Latte
 	dCtx.contextRegisters = contextRegisters;
 	dCtx.contextRegistersNew = (LatteContextRegister*)contextRegisters;
 	output->shaderType = shaderType;
+    // La variable writesPointSize fue eliminada para compatibilidad con Android
 }
+
 
 void LatteDecompiler_DecompileVertexShader(uint64 shaderBaseHash, uint32* contextRegisters, uint8* programData, uint32 programSize, struct LatteFetchShader* fetchShader, LatteDecompilerOptions& options, LatteDecompilerOutput_t* output)
 {
